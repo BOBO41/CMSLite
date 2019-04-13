60f3f02ccd33eec686d3e0f5d9a503fb0b29d063
liudexiang3218 <95316219@qq.com>
2019-04-14 01:03:29 +0800

    增加cms页面发送信息模块

--- src/main/webapp/vendor/custom.js
+++ src/main/webapp/vendor/custom.js
@@ -1,4 +1,27 @@
-
+$(document).ready(function() {
+	$("#sendMessageButton").click(function(event) {
+		var form=$("#contactForm")[0];
+		form.classList.remove('was-validated');
+		if (form.checkValidity() === false) {
+	          event.preventDefault();
+	          event.stopPropagation();
+	          form.classList.add('was-validated');
+	        }else{
+	    		var data=JSON.stringify(getMessageFormData());
+	    		postCmsFormData("message","add",getMessageFormData());
+	    		form.reset();
+	        }
+	});
+});
+function getMessageFormData() {
+	var json = {
+		"name" : $("#inputName").val(),
+		"email" : $("#inputEmail").val(),
+		"mobile" : $("#inputMobile").val(),
+		"content" : $("#inputContent").val()
+	};
+	return json;
+}
 function onKeyDown(event) {
 	var e = event || window.event || arguments.callee.caller.arguments[0];
 	if (e && e.keyCode == 27) { // Esc
@@ -10,4 +33,30 @@ function onKeyDown(event) {
 	if (e && e.keyCode == 13) { // enter
 		$("#searchform").submit();
 	}
+}
+function postCmsFormData(table,action,json) {
+	var data=JSON.stringify(json);
+	$.ajax({
+		contentType : "application/json",
+		type : 'POST',
+		url : "/cms/api/"+table+"/"+action,
+		data : data,
+		dataType : "json",
+		success : function(response) {
+			if(response.success){
+				showMessage('success','Success',"We've got your note, and we will give a reply as soon as possible. Please check your E-mail.");
+			}else{
+				showMessage('danger','Error',response.message);
+			}
+		},
+		error : function(response) {
+			showMessage('danger','Error',response.message);
+		}
+	});
+}
+function showMessage(type,title,content){
+	$('#modalLongTitle').html(title);
+	$('#modalBody').html(content);
+	$('#modalAlert').addClass("alert-"+type);
+	$('#modalCenter').modal('show');
 }
