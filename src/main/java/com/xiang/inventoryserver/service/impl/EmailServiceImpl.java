c446969bb36980a8481ac51bdef454b3760e8890
liudexiang3218 <95316219@qq.com>
2019-04-15 23:19:06 +0800

    完善邮件发送

--- src/main/java/com/xiang/inventoryserver/service/impl/EmailServiceImpl.java
+++ src/main/java/com/xiang/inventoryserver/service/impl/EmailServiceImpl.java
@@ -1,16 +1,28 @@
 package com.xiang.inventoryserver.service.impl;
 
+import org.simplejavamail.email.Email;
+
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.Mailer;
+import org.simplejavamail.springsupport.SimpleJavaMailSpringSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
 
 import com.xiang.inventoryserver.service.EmailService;
+@Service("emailService")
 @EnableAsync
+@Import(SimpleJavaMailSpringSupport.class)
 public class EmailServiceImpl implements EmailService{
-
+	@Autowired
+    private Mailer mailer;
 	@Async
 	@Override
 	public void replyMessage(String email,String content) {
-		
+		Email emailBuilder = EmailBuilder.startingBlank().to(email).withSubject("test").withHTMLText(content).buildEmail();
+		mailer.sendMail(emailBuilder);
 	}
 
 }
