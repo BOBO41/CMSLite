85de24af6787e76036b84d942e762b549c3ceb61
liudexiang3218 <95316219@qq.com>
2019-04-16 00:34:31 +0800

    完善客户留言email通知

--- src/main/java/com/xiang/inventoryserver/service/impl/EmailServiceImpl.java
+++ src/main/java/com/xiang/inventoryserver/service/impl/EmailServiceImpl.java
@@ -1,17 +1,21 @@
 package com.xiang.inventoryserver.service.impl;
 
 import org.simplejavamail.email.Email;
-
 import org.simplejavamail.email.EmailBuilder;
 import org.simplejavamail.mailer.Mailer;
 import org.simplejavamail.springsupport.SimpleJavaMailSpringSupport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 import com.xiang.inventoryserver.service.EmailService;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 @Service("emailService")
 @EnableAsync
 @Import(SimpleJavaMailSpringSupport.class)
@@ -20,9 +24,10 @@ public class EmailServiceImpl implements EmailService{
     private Mailer mailer;
 	@Async
 	@Override
+	@Cacheable(value = "replyMessageCache", key = "#email")
 	public void replyMessage(String email,String content) {
-		Email emailBuilder = EmailBuilder.startingBlank().to(email).withSubject("test").withHTMLText(content).buildEmail();
-		mailer.sendMail(emailBuilder);
+			Email emailBuilder = EmailBuilder.startingBlank().to(email).withSubject("test").withHTMLText(content).buildEmail();
+			mailer.sendMail(emailBuilder);
 	}
 
 }
