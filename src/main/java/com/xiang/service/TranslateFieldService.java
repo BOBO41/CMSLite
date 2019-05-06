0154ad4df8cb9917029e7922ebfb046b59fe3966
liudexiang3218 <95316219@qq.com>
2019-05-06 22:08:38 +0800

    删除多余无用自动生成的代码

--- src/main/java/com/xiang/service/TranslateFieldService.java
+++
@@ -1,9 +0,0 @@
-package com.xiang.service;
-
-import com.xiang.bean.po.TranslateField;
-
-public interface TranslateFieldService extends BaseService<TranslateField>{
-	public TranslateField get(Long id);
-	public void save(TranslateField record);
-	public void update(TranslateField record);
-}
